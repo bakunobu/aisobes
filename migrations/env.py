ed1ed34b3cff5c22c4a94a7c313ea159edb26622
Sergei Vakunov <bakunobu@gmail.com>
2026-06-12 22:54:03 +0300

    model update

--- migrations/env.py
+++ migrations/env.py
@@ -10,11 +10,16 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app  # Import monolithic app instance
 
+# This is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# Set sqlalchemy.url from app's database configuration
 with app.app_context():
-    config = context.config
-    fileConfig(config.config_file_name)
-    
-    # Set sqlalchemy.url from app's database configuration
     config.set_main_option('sqlalchemy.url', 
         current_app.extensions['migrate'].db.engine.url.render_as_string(hide_password=False))
     
@@ -39,16 +44,17 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode."""
-    connectable = target_db.engine
+    with app.app_context():  # Add app context here
+        connectable = target_db.engine
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_db.metadata
-        )
+        with connectable.connect() as connection:
+            context.configure(
+                connection=connection,
+                target_metadata=target_db.metadata
+            )
 
-        with context.begin_transaction():
-            context.run_migrations()
+            with context.begin_transaction():
+                context.run_migrations()
 
 
 if context.is_offline_mode():
